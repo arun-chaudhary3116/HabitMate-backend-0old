ad35df33c7cc366e4349876012ab678aac83b002
Arun Chaudhary <arunc3116@gmail.com>
2025-10-04 13:33:24 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false,
+      secure: true,
       httpOnly: true,
       sameSite: "lax",
     }, // set secure: true if using HTTPS
