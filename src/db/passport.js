9abb60ce5c2b4fcf4249286ed959d73fd605c8a4
Arun Chaudhary <arunc3116@gmail.com>
2025-10-04 13:22:07 +0100

    Update passport.js

--- src/db/passport.js
+++ src/db/passport.js
@@ -17,7 +17,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:8000/api/v2/users/google/callback",
+      callbackURL: `${process.env.BACKEND_URL}/api/v1/users/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
